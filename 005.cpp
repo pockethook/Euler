62b2db78b72476f077f42415b9066c13e0281c01
Cheehan <pockethook@npyu.eu>
2014-09-27 11:52:55 +0100

    Formatting.

--- 005.cpp
+++ 005.cpp
@@ -1,37 +1,41 @@
 // smallest positive number evenly divisible by all the numbers from 1 to 20
 
-#include <iostream> 
+#include <iostream>
 
-using namespace std;
+using std::cout;
+using std::endl;
+
+unsigned long factorial(const unsigned n) {
+	unsigned long fact = 1;
+	for (unsigned i = 1; i < n; ++i) {
+		fact *= i;
+	}
+	return fact;
+}
 
 int main() {
 
-  long max = 1;
-  // largest number product of all
-  for (int i = 2; i < 21; i++)
-    max*=i;
+	unsigned limit {20};
+	unsigned long largest {factorial(limit)};
 
-  long smallest;
+	long smallest {0};
 
-  for (long i = 1; i <= max; i++) {
-    bool flag = true;
+	for (unsigned long trial = 2; trial <= largest; ++trial) {
+		bool divisible {true};
+		for (unsigned divisor = 2; divisor <= limit; ++divisor) {
+			if (trial % divisor != 0) {
+				divisible = false;
+				break;
+			}
+		}
 
-    // for each divisor
-    for (int j = 2; j < 21; j++)
-      // if not divisible try next highest number
-      if (i%j!=0) {
-	flag = false;
-	break;
-      }
-  
-    // if divisible by all numbers break loop
-    if (flag) {
-      smallest = i;
-      break;
-    }   
-  }
-  
-  cout << smallest << endl;
-  
-  return 0; 
+		if (divisible) {
+			smallest = trial;
+			break;
+		}
+	}
+
+	cout << smallest << endl;
+
+	return 0;
 }
